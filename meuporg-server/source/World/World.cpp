f4b1498322192a609c152f4b05e889f7628bdcea
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-06-06 22:55:25 +0200

    Ajout de l'ECS pour le serveur

--- meuporg-server/source/World/World.cpp
+++ meuporg-server/source/World/World.cpp
@@ -6,6 +6,21 @@ World::World()
 
 World::~World()
 {
+    // Free the memory.
+    for(kantan::Entity* e : m_entities)
+        delete e;
+
+    for(kantan::DeletionMarkerComponent* dmc : m_deletionMarkerComponents)
+        delete dmc;
+
+    for(kantan::PolygonHitboxComponent* phc : m_polygonHitboxComponents)
+        delete phc;
+
+    for(kantan::MovementComponent* mc : m_movementComponents)
+        delete mc;
+
+    for(kantan::RotationComponent* rc : m_rotationComponents)
+        delete rc;
 }
 
 void World::init()
@@ -15,5 +30,101 @@ void World::init()
 
 void World::update(sf::Time dt)
 {
+    // Rotations.
+    m_rotationSystem.update(dt, m_polygonHitboxComponents, m_rotationComponents);
 
+    // Collision.
+    m_collisionSystem.update(dt, m_polygonHitboxComponents, m_movementComponents);
+
+    // Clean the entities.
+    cleanEntities();
 }
+
+void World::cleanEntities()
+{
+    for(std::size_t i(0) ; i < m_deletionMarkerComponents.size() ;)
+    {
+        kantan::DeletionMarkerComponent* dmc = m_deletionMarkerComponents[i];
+
+        if(dmc->marked)
+        {
+            auto itr = std::find_if(m_entities.begin(), m_entities.end(), [dmc](kantan::Entity* e){
+                            return dmc->getOwnerId() == e->getId();
+                        });
+
+            if(itr != m_entities.end())
+            {
+                kantan::Entity* entity = (*itr);
+                std::unordered_map<std::string, kantan::Component*> components = entity->getAllComponents();
+
+                for(std::pair<std::string, kantan::Component*> componentPair : components)
+                {
+                    kantan::Component* component = componentPair.second;
+
+                    if(component->getName() == "DeletionMarker")
+                        removeComponentFrom<kantan::DeletionMarkerComponent>(component, m_deletionMarkerComponents);
+                    else if(component->getName() == "PolygonHitbox")
+                        removeComponentFrom<kantan::PolygonHitboxComponent>(component, m_polygonHitboxComponents);
+                    else if(component->getName() == "Movement")
+                        removeComponentFrom<kantan::MovementComponent>(component, m_movementComponents);
+                    else if(component->getName() == "Rotation")
+                        removeComponentFrom<kantan::RotationComponent>(component, m_rotationComponents);
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+kantan::Entity* World::createEntity(std::string name)
+{
+    kantan::Entity* e = new kantan::Entity(name);
+
+    kantan::DeletionMarkerComponent* dmc = createDeletionMarkerComponent(e->getId());
+    e->addComponent(dmc);
+
+    m_entities.push_back(e);
+
+    return e;
+}
+
+// createXXXComponent methods.
+kantan::DeletionMarkerComponent* World::createDeletionMarkerComponent(std::size_t ownerId)
+{
+    kantan::DeletionMarkerComponent* dmc = new kantan::DeletionMarkerComponent(ownerId);
+    m_deletionMarkerComponents.push_back(dmc);
+
+    return dmc;
+}
+
+kantan::PolygonHitboxComponent* World::createPolygonHitboxComponent(std::size_t ownerId)
+{
+    kantan::PolygonHitboxComponent* phc = new kantan::PolygonHitboxComponent(ownerId);
+    m_polygonHitboxComponents.push_back(phc);
+
+    return phc;
+}
+
+kantan::MovementComponent* World::createMovementComponent(std::size_t ownerId)
+{
+    kantan::MovementComponent* mc = new kantan::MovementComponent(ownerId);
+    m_movementComponents.push_back(mc);
+
+    return mc;
+}
+
+kantan::RotationComponent* World::createRotationComponent(std::size_t ownerId)
+{
+    kantan::RotationComponent* rc = new kantan::RotationComponent(ownerId);
+    m_rotationComponents.push_back(rc);
+
+    return rc;
+}
+
--- meuporg-server/source/World/World.hpp
+++ meuporg-server/source/World/World.hpp
@@ -3,6 +3,8 @@
 
 #include <SFML/System.hpp>
 
+#include "../Kantan-2/kantan.hpp"
+
 /*
     World class.
     Manages the world for a single unit.
@@ -23,6 +25,53 @@ class World
         void update(sf::Time dt);
 
     protected:
+        // Removes the entities marked as to delete.
+        void cleanEntities();
+
+        // Removes a component from the component list.
+        template<typename T>
+        bool removeComponentFrom(kantan::Component* c, std::vector<T*>& components);
+
+        // Creates an entity with only the deletion marker component.
+        kantan::Entity* createEntity(std::string name);
+
+        // createXXXComponent methods.
+        kantan::DeletionMarkerComponent* createDeletionMarkerComponent(std::size_t ownerId);
+        kantan::PolygonHitboxComponent* createPolygonHitboxComponent(std::size_t ownerId);
+        kantan::MovementComponent* createMovementComponent(std::size_t ownerId);
+        kantan::RotationComponent* createRotationComponent(std::size_t ownerId);
+
+        // createXXX methods.
+
+    protected:
+        // Entities.
+        std::vector<kantan::Entity*> m_entities;
+
+        // Components.
+        std::vector<kantan::DeletionMarkerComponent*> m_deletionMarkerComponents;
+        std::vector<kantan::PolygonHitboxComponent*> m_polygonHitboxComponents;
+        std::vector<kantan::MovementComponent*> m_movementComponents;
+        std::vector<kantan::RotationComponent*> m_rotationComponents;
+
+        // Systems.
+        kantan::PolygonCollisionSystem m_collisionSystem;
+        kantan::RotationSystem m_rotationSystem;
 };
+
+template<typename T>
+bool World::removeComponentFrom(kantan::Component* c, std::vector<T*>& components)
+{
+    auto itr = std::find_if(components.begin(), components.end(), [c](kantan::Component* nc){
+                                    return c == nc;
+                                });
+
+    if(itr != components.end())
+    {
+        components.erase(itr);
+        return true;
+    }
+
+    return false;
+}
 
 #endif // WORLD_HPP_INCLUDED
