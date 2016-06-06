54cd102aea1d348581da45435be596dc2aa423df
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-06-06 22:22:14 +0200

    Ajout d'utilitaire pour les paquets (serveur)

---
+++ meuporg-server/source/Utils/SFMLPacketUtils.cpp
@@ -0,0 +1,18 @@
+#include "SFMLPacketUtils.hpp"
+
+// Reading.
+sf::Packet& operator >>(sf::Packet& packet, sf::Time& time)
+{
+    sf::Int32 x;
+    packet >> x;
+
+    time = sf::milliseconds( x);
+
+    return packet;
+}
+
+// Writing.
+sf::Packet& operator <<(sf::Packet& packet, sf::Time time)
+{
+    return packet << time.asMilliseconds();
+}
