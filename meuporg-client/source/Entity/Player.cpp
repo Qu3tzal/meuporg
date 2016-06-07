e4c1d75db6a39c9346c64188820502576e0fa319
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-06-07 03:36:17 +0200

    Corrections de l'animation

--- meuporg-client/source/Entity/Player.cpp
+++ meuporg-client/source/Entity/Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.hpp"
 
-Player::Player(kantan::TextureHolder* textures, std::string name, unsigned int id) : Entity(id, name, Entity::Type::PLAYER, textures)
+Player::Player(kantan::TextureHolder* textures, std::string name, unsigned int id)
+    : Entity(id, name, Entity::Type::PLAYER, textures)
+    , direction(Direction::DOWN)
 {
 
 }
@@ -21,7 +23,7 @@ void Player::init()
     a_MoveDown.addTextRect(sf::IntRect(144, 8, 31, 48));
     a_MoveDown.addTextRect(sf::IntRect(208, 9, 30, 47));
     a_MoveDown.setLoop(true);
-
+    a_MoveDown.setSpeed(12);
 
     // Move up animation
     a_MoveUp.addTextRect(sf::IntRect(16, 200, 31, 48));
@@ -29,6 +31,7 @@ void Player::init()
     a_MoveUp.addTextRect(sf::IntRect(144, 200, 31, 48));
     a_MoveUp.addTextRect(sf::IntRect(210, 201, 26, 47));
     a_MoveUp.setLoop(true);
+    a_MoveUp.setSpeed(12);
 
     // Move Left animation
     a_MoveLeft.addTextRect(sf::IntRect(22, 72, 22, 48));
@@ -36,6 +39,7 @@ void Player::init()
     a_MoveLeft.addTextRect(sf::IntRect(150, 72, 22, 48));
     a_MoveLeft.addTextRect(sf::IntRect(212, 73, 27, 47));
     a_MoveLeft.setLoop(true);
+    a_MoveLeft.setSpeed(12);
 
     // Move Right animation
     a_MoveRight.addTextRect(sf::IntRect(20, 136, 22, 48));
@@ -43,10 +47,13 @@ void Player::init()
     a_MoveRight.addTextRect(sf::IntRect(148, 136, 22, 48));
     a_MoveRight.addTextRect(sf::IntRect(209, 137, 27, 47));
     a_MoveRight.setLoop(true);
+    a_MoveRight.setSpeed(12);
 }
 
 void Player::update(sf::Time dt)
 {
+    Entity::update(dt);
+
     setDirection();
     switch(direction)
     {
@@ -112,11 +119,17 @@ void Player::update(sf::Time dt)
 
             sprite.setTextureRect(a_MoveRight.getFrame().first);
             break;
+        default:
+            sprite.setTextureRect(a_MoveDown.getFrame().first);
+            break;
     }
 }
 
 void Player::setDirection()
 {
+    if(velocity == sf::Vector2f(0.f, 0.f))
+        return;
+
     velocityNormalized = Entity::normalize(velocity);
 
     if(std::abs(velocityNormalized.x) > std::abs(velocityNormalized.y))
