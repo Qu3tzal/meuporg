c8599fb9fb5296640778b74a39fcef808602870a
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-06-29 19:11:44 +0200

    Ajout de méthode dans la classe mission

--- meuporg-client/source/Entity/Mission.cpp
+++ meuporg-client/source/Entity/Mission.cpp
@@ -1,11 +1,49 @@
 #include "Mission.hpp"
 
-Mission::Mission()
+Mission::Mission(unsigned int id)
 {
-    //ctor
+    this->missionId = id;
 }
 
 Mission::~Mission()
 {
     //dtor
+}
+
+void Mission::init()
+{
+    switch(missionId)
+    {
+    case 0 :
+        addText("Bonjour, Je m'appele Pnice et je suis là pour te la mettre !")
+        addText("J'ai une mission à te proposer ! Serait tu capables d'enculer l'enculeur de mouche ?");
+    }
+}
+
+void Mission::addText(std::string str)
+{
+    text.push_back(str);
+}
+
+unsigned int Mission::getMissionId() const
+{
+    return this->missionId;
+}
+
+std::string Mission::getText() const
+{
+    return text[index];
+}
+
+void Mission::nextText()
+{
+    if(index <= text.size() - 1)
+    {
+        index++;
+    }
+}
+
+void Mission::resetIndex()
+{
+    index = 0;
 }
--- meuporg-client/source/Entity/Mission.hpp
+++ meuporg-client/source/Entity/Mission.hpp
@@ -5,10 +5,31 @@
 class Mission
 {
     public:
-        Mission();
+        Mission(unsigned int missionId);
+
         virtual ~Mission();
 
+        void init();
+
+        unsigned int getMissionId() const;
+
+        void addText(std::string str);
+
+        std::string getText() const;
+
+        void nextText();
+
+        void resetIndex();
+
     protected:
+
+        unsigned int missionId;
+
+        std::vector<std::string> text;
+
+        // the text index
+        unsigned int index;
+
 
     private:
 };
