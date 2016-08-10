0db844d9b8fbbfe7235c5bd0f34bbd818cf63a5d
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-08-10 18:27:13 +0200

    Ajout de differente methode dans Dialog

--- meuporg-client/source/Gui/Dialog.cpp
+++ meuporg-client/source/Gui/Dialog.cpp
@@ -1,11 +1,43 @@
 #include "Dialog.hpp"
 
-Dialog::Dialog()
+Dialog::Dialog(kantan::FontHolder* fonts)
 {
-    //ctor
+    this->fonts = fonts;
 }
 
 Dialog::~Dialog()
 {
     //dtor
+}
+
+void Dialog::init()
+{
+    background.setFillColor(sf::Color(60, 60, 60, 60));
+
+    text.setFont(fonts->get(ResourceId::MONOF_56));
+    text.setCharacterSize(16);
+    text.setColor(sf::Color::White);
+}
+
+void Dialog::update(sf::Time dt)
+{
+
+}
+
+void Dialog::handleEvent(sf::Event e)
+{
+
+}
+
+void Dialog::setText(std::vector<std::string> texts)
+{
+    this->texts = texts;
+}
+
+void Dialog::draw(sf::RenderTarget& window, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    window.draw(background);
+    window.draw(text);
 }
--- meuporg-client/source/Gui/Dialog.hpp
+++ meuporg-client/source/Gui/Dialog.hpp
@@ -1,16 +1,41 @@
 #ifndef DIALOG_HPP
 #define DIALOG_HPP
 
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
 
-class Dialog
+#include "../ResourceHolder.hpp"
+#include "../ResourceId.hpp"
+
+
+class Dialog : public sf::Drawable, public sf::Transformable
 {
     public:
-        Dialog();
+        Dialog(kantan::FontHolder* fonts);
         virtual ~Dialog();
+
+        void init();
+
+        void update(sf::Time dt);
+
+        void handleEvent(sf::Event e);
+
+        void setText(std::vector<std::string> text);
 
     protected:
 
+        virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
+
     private:
+
+        kantan::FontHolder* fonts;
+
+        sf::RectangleShape background;
+
+        sf::Text text;
+
+        std::vector<std::string> texts;
 };
 
 #endif // DIALOG_HPP
