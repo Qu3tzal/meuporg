c5f01623c2d1d28da279fe1b968e60826b810e1c
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-08-15 22:49:02 +0200

    Dialog

--- meuporg-client/source/Gui/Dialog.cpp
+++ meuporg-client/source/Gui/Dialog.cpp
@@ -1,6 +1,7 @@
 #include "Dialog.hpp"
 
-Dialog::Dialog(kantan::FontHolder* fonts)
+Dialog::Dialog(kantan::FontHolder* fonts) : arrow("->")
+    ,itr(0)
 {
     this->fonts = fonts;
 }
@@ -13,6 +14,7 @@ Dialog::~Dialog()
 void Dialog::init()
 {
     background.setFillColor(sf::Color(60, 60, 60, 60));
+    background.setSize(sf::Vector2f(100, 50));
 
     text.setFont(fonts->get(ResourceId::MONOF_56));
     text.setCharacterSize(16);
@@ -27,17 +29,56 @@ void Dialog::update(sf::Time dt)
 void Dialog::handleEvent(sf::Event e)
 {
 
+    if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Return)
+    {
+        next();
+    }
 }
 
-void Dialog::setText(std::vector<std::string> texts)
+void Dialog::next()
 {
-    this->texts = texts;
+    if(itr < texts.size())
+    {
+        text.setString(texts[itr]);
+        itr++;
+    }
+    if(itr == texts.size()-1)
+    {
+
+    }
+}
+
+void Dialog::setText(std::string str)
+{
+    unsigned int maxSize(40);
+    unsigned int maxLine(3);
+
+    if(str.length() >= maxSize)
+    {
+        unsigned int nb = 1;
+        unsigned int last = 0;
+        for(unsigned int j = 1 ; j <= (unsigned int)(str.length()/maxSize); j++)
+        {
+            str.insert(j * maxSize , "\n");
+            nb++;
+            if(nb == maxLine)
+            {
+                texts.push_back(str.substr((j - 1) * maxSize, (j * maxSize + 1) - ((j - 1) * maxSize)));
+                nb = 0;
+                last = j * maxSize + 1;
+            }
+        }
+        texts.push_back(str.substr(last, str.size() - last));
+
+    }
+    next();
+
 }
 
 void Dialog::draw(sf::RenderTarget& window, sf::RenderStates states) const
 {
     states.transform *= getTransform();
 
-    window.draw(background);
-    window.draw(text);
+    window.draw(background, states);
+    window.draw(text, states);
 }
--- meuporg-client/source/Gui/Dialog.hpp
+++ meuporg-client/source/Gui/Dialog.hpp
@@ -4,15 +4,23 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <vector>
+#include <map>
+#include <functional>
 
 #include "../ResourceHolder.hpp"
 #include "../ResourceId.hpp"
 
+struct DialogChoice
+{
+    std::map<int, std::string> possibilities;
+    std::function<void(int)> callback;
+};
 
 class Dialog : public sf::Drawable, public sf::Transformable
 {
     public:
         Dialog(kantan::FontHolder* fonts);
+
         virtual ~Dialog();
 
         void init();
@@ -21,11 +29,15 @@ class Dialog : public sf::Drawable, public sf::Transformable
 
         void handleEvent(sf::Event e);
 
-        void setText(std::vector<std::string> text);
+        void setText(std::string str);
+
+        void setChoice(DialogChoice choice);
 
     protected:
 
         virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
+
+        std::string arrow;
 
     private:
 
@@ -35,7 +47,13 @@ class Dialog : public sf::Drawable, public sf::Transformable
 
         sf::Text text;
 
+        void next();
+
+        unsigned int itr;
+
         std::vector<std::string> texts;
+
+        DialogChoice choice;
 };
 
 #endif // DIALOG_HPP
