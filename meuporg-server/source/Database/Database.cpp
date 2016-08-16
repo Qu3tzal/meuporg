7d022339376e264afe4bf19e2ac69e31ed890cab
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2016-08-16 23:18:46 +0200

    Ajout de méthodes à la classe Database

--- meuporg-server/source/Database/Database.cpp
+++ meuporg-server/source/Database/Database.cpp
@@ -5,7 +5,7 @@ Database::Database(std::string filepath)
     m_opened = false;
 
     // Check error.
-    int error = sqlite3_open(filepath.c_str(), m_db);
+    int error = sqlite3_open(filepath.c_str(), &m_db);
 
     if(error == SQLITE_OK)
     {
@@ -21,10 +21,87 @@ Database::~Database()
 
 bool Database::checkAccountPassword(std::string username, std::string password)
 {
+    // Prepare statement.
+    std::string statementString("SELECT id FROM `players` WHERE username=:username AND hashedPassword=:hashedPassword");
 
+    sqlite3_stmt* statement;
+    int error = sqlite3_prepare(m_db, statementString.c_str(), statementString.size(), &statement, nullptr);
+
+    if(error != SQLITE_OK)
+        return false;
+
+    std::string hashedPassword(""); /// /!\ Hash the password.
+
+    // Bind parameters.
+    int usernameParameterIndex = sqlite3_bind_parameter_index(statement, ":username");
+    int hashedPasswordParameterIndex = sqlite3_bind_parameter_index(statement, ":hashedPassword");
+    sqlite3_bind_text(statement, usernameParameterIndex, username.c_str(), username.size(), nullptr);
+    sqlite3_bind_text(statement, hashedPasswordParameterIndex, hashedPassword.c_str(), hashedPassword.size(), nullptr);
+
+    // Execute.
+    while(true)
+    {
+        int status = sqlite3_step(statement);
+
+        if(status == SQLITE_BUSY)
+            continue;
+
+        bool result = (status == SQLITE_ROW);
+
+        // Free.
+        sqlite3_finalize(statement);
+        return result;
+    }
 }
 
 PlayerData Database::getPlayerStats(std::string username)
 {
+    // Prepare statement.
+    std::string statementString("SELECT * FROM `players` WHERE username=:username");
 
+    sqlite3_stmt* statement;
+    int error = sqlite3_prepare(m_db, statementString.c_str(), statementString.size(), &statement, nullptr);
+
+    if(error != SQLITE_OK)
+        return PlayerData();
+
+    // Bind parameters.
+    int usernameParameterIndex = sqlite3_bind_parameter_index(statement, ":username");
+    sqlite3_bind_text(statement, usernameParameterIndex, username.c_str(), username.size(), nullptr);
+
+    // Execute.
+    while(true)
+    {
+        int status = sqlite3_step(statement);
+
+        if(status == SQLITE_BUSY)
+            continue;
+
+        break;
+    }
+
+    PlayerData playerData;
+
+    playerData.dbid = sqlite3_column_int(statement, 0);
+
+    playerData.username = std::string(reinterpret_cast<const char*>(sqlite3_column_text(statement, 1)));
+    playerData.hashedPassword = std::string(reinterpret_cast<const char*>(sqlite3_column_text(statement, 2)));
+
+    playerData.worldId = sqlite3_column_int(statement, 3);
+    playerData.positionX = sqlite3_column_double(statement, 4);
+    playerData.positionY = sqlite3_column_double(statement, 5);
+
+    playerData.hp = sqlite3_column_double(statement, 6);
+    playerData.maxhp = sqlite3_column_double(statement, 7);
+    playerData.strength = sqlite3_column_double(statement, 8);
+    playerData.agility = sqlite3_column_double(statement, 9);
+    playerData.resistance = sqlite3_column_double(statement, 10);
+
+    playerData.xp = sqlite3_column_double(statement, 11);
+    playerData.level = sqlite3_column_double(statement, 12);
+
+    // Free.
+    sqlite3_finalize(statement);
+
+    return playerData;
 }
