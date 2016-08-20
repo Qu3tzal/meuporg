20c8591df74c23729086422aa70d55c83332c940
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-08-20 13:33:30 +0200

    Ajout d'une nouvelle texture pour la balle

--- meuporg-client/source/Game/Game.cpp
+++ meuporg-client/source/Game/Game.cpp
@@ -21,7 +21,7 @@ Game::Game(sf::RenderWindow* window) : running(true)
     textures.load(ResourceId::ONE_PUNCH_MAN_TEXTURE, "assets/saitama_spritesheet.png");
     textures.load(ResourceId::TILESET, "assets/box.png");
 
-    textures.load(ResourceId::BULLET, "assets/bullet.png");
+    textures.load(ResourceId::BULLET, "assets/bullet2.png");
 
     this->window = window;
 }
