cda141416225af585c519f2fa28b825bc32ff0de
Louis Babuchon <louis.babuchon@etu.univ-tours.fr>
2016-08-23 16:29:57 +0200

    Le fond est maintenant noir

--- meuporg-client/source/Game/Game.cpp
+++ meuporg-client/source/Game/Game.cpp
@@ -313,7 +313,7 @@ void Game::EventHandle(sf::Event event)
 
 void Game::render(sf::RenderWindow* window)
 {
-    window->clear(sf::Color::White);
+    window->clear(sf::Color::Black);
     switch(state)
      {
          case State::MENU:
