057d25f321a339f7b19b1d945c9ff8afdd407dd2
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-06-25 11:15:40 +0200

    Corrected forgotten 'const' qualifier

--- meuporg-server/source/Utils/SFMLPacketUtils.hpp
+++ meuporg-server/source/Utils/SFMLPacketUtils.hpp
@@ -11,7 +11,7 @@ template<typename T>
 sf::Packet& operator >>(sf::Packet& packet, sf::Vector2<T>& vector);
 
 // Writing.
-sf::Packet& operator <<(sf::Packet& packet, std::size_t& x);
+sf::Packet& operator <<(sf::Packet& packet, const std::size_t& x);
 sf::Packet& operator <<(sf::Packet& packet, const sf::Time& time);
 template<typename T>
 sf::Packet& operator <<(sf::Packet& packet, const sf::Vector2<T>& vector);
