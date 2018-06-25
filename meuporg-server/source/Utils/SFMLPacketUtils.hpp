a5b63b1b123ab36d840cbb6c5355537ca1615cc3
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-06-25 11:11:00 +0200

    Add an overload for forcing std::size_t to be 32 bits on the network

--- meuporg-server/source/Utils/SFMLPacketUtils.hpp
+++ meuporg-server/source/Utils/SFMLPacketUtils.hpp
@@ -5,11 +5,13 @@
 #include <SFML/Network.hpp>
 
 // Reading.
+sf::Packet& operator >>(sf::Packet& packet, std::size_t& x);
 sf::Packet& operator >>(sf::Packet& packet, sf::Time& time);
 template<typename T>
 sf::Packet& operator >>(sf::Packet& packet, sf::Vector2<T>& vector);
 
 // Writing.
+sf::Packet& operator <<(sf::Packet& packet, std::size_t& x);
 sf::Packet& operator <<(sf::Packet& packet, const sf::Time& time);
 template<typename T>
 sf::Packet& operator <<(sf::Packet& packet, const sf::Vector2<T>& vector);
