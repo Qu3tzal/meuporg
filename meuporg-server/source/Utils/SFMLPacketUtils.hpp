f2cbed4dbff80ceaabc87f0262253bec205e9fdb
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-06-26 14:06:11 +0200

    Prototype surcharge types 64 bits

--- meuporg-server/source/Utils/SFMLPacketUtils.hpp
+++ meuporg-server/source/Utils/SFMLPacketUtils.hpp
@@ -5,12 +5,14 @@
 #include <SFML/Network.hpp>
 
 // Reading.
+sf::Packet& operator >>(sf::Packet& packet, unsigned long const& x);
 sf::Packet& operator >>(sf::Packet& packet, std::size_t& x);
 sf::Packet& operator >>(sf::Packet& packet, sf::Time& time);
 template<typename T>
 sf::Packet& operator >>(sf::Packet& packet, sf::Vector2<T>& vector);
 
 // Writing.
+sf::Packet& operator <<(sf::Packet& packet, unsigned long const& x);
 sf::Packet& operator <<(sf::Packet& packet, const std::size_t& x);
 sf::Packet& operator <<(sf::Packet& packet, const sf::Time& time);
 template<typename T>
