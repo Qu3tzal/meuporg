161d8b2bf69e145abcd459f11d06260f1862f8c7
Maxime Alvarez <maxime.alvarez@etu.univ-tours.fr>
2018-06-30 11:48:59 +0200

    Suppression de l'overload sf::Packet <</>>(std::size_t)
    Une fois la SFML et le projet compilé en 64 bits avec l'option -m64, plus de problème c'est bien sf::Uint64 qui est utilisé.

--- meuporg-server/source/Utils/SFMLPacketUtils.cpp
+++ meuporg-server/source/Utils/SFMLPacketUtils.cpp
@@ -1,16 +1,6 @@
 #include "SFMLPacketUtils.hpp"
 
 // Reading.
-sf::Packet& operator >>(sf::Packet& packet, std::size_t& x)
-{
-    sf::Uint32 ui32b;
-    packet >> ui32b;
-    
-    x = (std::size_t)(ui32b);
-    
-    return packet;
-}
-
 sf::Packet& operator >>(sf::Packet& packet, sf::Time& time)
 {
     sf::Int32 x;
@@ -22,14 +12,6 @@ sf::Packet& operator >>(sf::Packet& packet, sf::Time& time)
 }
 
 // Writing.
-sf::Packet& operator <<(sf::Packet& packet, const std::size_t& x)
-{
-    sf::Uint32 ui32b;
-    ui32b = (unsigned int)(x);
-    
-    return packet << ui32b;
-}
-
 sf::Packet& operator <<(sf::Packet& packet, const sf::Time& time)
 {
     return packet << time.asMilliseconds();
--- meuporg-server/source/Utils/SFMLPacketUtils.hpp
+++ meuporg-server/source/Utils/SFMLPacketUtils.hpp
@@ -5,13 +5,11 @@
 #include <SFML/Network.hpp>
 
 // Reading.
-sf::Packet& operator >>(sf::Packet& packet, std::size_t& x);
 sf::Packet& operator >>(sf::Packet& packet, sf::Time& time);
 template<typename T>
 sf::Packet& operator >>(sf::Packet& packet, sf::Vector2<T>& vector);
 
 // Writing.
-sf::Packet& operator <<(sf::Packet& packet, const std::size_t& x);
 sf::Packet& operator <<(sf::Packet& packet, const sf::Time& time);
 template<typename T>
 sf::Packet& operator <<(sf::Packet& packet, const sf::Vector2<T>& vector);
